99015060635005ae81f882f4abc16bac3f798ec6
dennis cheng <dennischeng520@gmail.com>
2023-09-12 10:29:40 +0800

    done

--- app.py
+++ app.py
@@ -13,36 +13,52 @@ st.markdown('''
 **Welcome to the Taxi Fare Prediction App!**
 ''')
 
+# Form for input
+with st.form(key='params_for_api'):
+    st.subheader("Enter Details")
+    pickup_date = st.date_input('Pickup Date', value=datetime.date(2023, 9, 12))
+    pickup_time = st.time_input('Pickup Time', value=datetime.time(10, 0))
+    pickup_longitude = st.number_input('Pickup Longitude', value=40.7614327)
+    pickup_latitude = st.number_input('Pickup Latitude', value=40.7614327)
+    dropoff_longitude = st.number_input('Dropoff Longitude', value=40.7614327)
+    dropoff_latitude = st.number_input('Dropoff Latitude', value=40.7614327)
+    passenger_count = st.number_input('Passenger Count', min_value=1, max_value=4, step=1, value=1)
 
-# with st.form(key='params_for_api')
-pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2023, 9, 12, 10, 00, 00))
-pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2023, 9, 12, 11, 00, 00))
-pickup_datetime = f'{pickup_date} {pickup_time}'
-pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-pickup_latitude = st.number_input('pickup latitude', value=40.7614327)
-dropoff_longitude = st.number_input('dropoff longitude', value=40.7614327)
-dropoff_latitude = st.number_input('dropoff latitude', value=40.7614327)
-passenger_count = st.number_input('passenger count', min_value=1, max_value=4, step=1, value=1)
+    submitted = st.form_submit_button('Submit')
 
-params= dict(
-    pickup_datetime = pickup_datetime,
-    pickup_longitude = pickup_longitude,
-    pickup_latitude = pickup_latitude,
-    dropoff_longitude=dropoff_longitude,
-    dropoff_latitude=dropoff_latitude,
-    passenger_count = passenger_count
-)
+if submitted:
+    # Create a datetime object
+    pickup_datetime = datetime.datetime.combine(pickup_date, pickup_time)
 
+    params = {
+        'pickup_datetime': pickup_datetime.isoformat(),
+        'pickup_longitude': pickup_longitude,
+        'pickup_latitude': pickup_latitude,
+        'dropoff_longitude': dropoff_longitude,
+        'dropoff_latitude': dropoff_latitude,
+        'passenger_count': passenger_count
+    }
 
-url = 'https://taxifare-d6yophmzga-ew.a.run.app/predict'  # this is my fast API address
-response = requests.get(url, params=params)
+    url = 'https://taxifare-d6yophmzga-ew.a.run.app/predict'  # your FastAPI endpoint
+    response = requests.get(url, params=params)
 
-prediction = response.json()
-pred = prediction['fare_amount']
+    if response.status_code == 200:
+        prediction = response.json()
+        pred = prediction['fare_amount']
 
-st.markdown('''
-            Returning Results
-''')
+        st.markdown('''
+                    Returning Results
+        ''')
 
-st.header(f'The Fare Amount: ${round(pred, 2)}')
-## Once we have these, let's call our API in order to retrieve a prediction
+        st.header(f'The Fare Amount: ${round(pred, 2)}')
+
+        # Display the input data for reference
+        st.subheader("Input Data")
+        st.write(f'Pickup Date and Time: {pickup_datetime}')
+        st.write(f'Pickup Longitude: {pickup_longitude}')
+        st.write(f'Pickup Latitude: {pickup_latitude}')
+        st.write(f'Dropoff Longitude: {dropoff_longitude}')
+        st.write(f'Dropoff Latitude: {dropoff_latitude}')
+        st.write(f'Passenger Count: {passenger_count}')
+    else:
+        st.error(f'Error: {response.status_code} - Failed to retrieve prediction from the API')
